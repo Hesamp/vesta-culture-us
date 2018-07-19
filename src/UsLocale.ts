8ad3d5bc901764d5c4211fc6d15264cf47bf9109
hbtb <bishehsary@gmail.com>
2018-07-19 09:16:26 +0430

    bug fix on weekday

--- src/UsLocale.ts
+++ src/UsLocale.ts
@@ -13,8 +13,8 @@ export const UsLocale: ILocale = {
     daysInMonth: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
     monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
     monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    weekDays: ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-    weekDaysShort: ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    weekDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    weekDaysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     am_pm: ['am', 'pm'],
     defaultDateFormat: 'Y/m/d',
     defaultDateTimeFormat: 'Y/m/d H:i'
